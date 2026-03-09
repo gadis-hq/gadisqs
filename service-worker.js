37a9d1f2cd311697fde059f095fa1e950d0fdef3
Gadis QS HQ <qsgadis449@gmail.com>
2026-03-09 14:16:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-self.addEventListener('install', e => { console.log('SW Installed'); });
-self.addEventListener('activate', e => { console.log('SW Activated'); });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
